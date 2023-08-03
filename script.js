aa8f1d341c73b42c8da2f51581046eb62e5b69bf
Chyisiang <chyisiang8@gmail.com>
2023-08-03 16:31:01 +0800

    audio and mute

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function subscribeNewsletter() {
-    // Get the user's email address
     const email = document.getElementById('email').value;
-
-    // You can implement your own logic here to subscribe the user to the newsletter.
-    // For this example, we'll just display a success message.
     const feedback = document.getElementById('newsletter-feedback');
     feedback.textContent = `Thank you for subscribing! You will receive updates at ${email}.`;
 }
 
 function submitFeedback() {
-    // Get the user's feedback
     const feedbackText = document.getElementById('feedback').value;
-
-    // You can implement your own logic here to handle the user's feedback.
-    // For this example, we'll just display a success message.
     const feedback = document.getElementById('newsletter-feedback');
     feedback.textContent = "Thank you for your feedback!";
+}
+
+function toggleMute() {
+    var audio = document.getElementById('background-audio');
+    audio.muted = !audio.muted;
+
+    var muteButton = document.getElementById('mute-button');
+    muteButton.classList.toggle('muted', audio.muted);
 }
